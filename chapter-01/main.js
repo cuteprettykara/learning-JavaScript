b9ed7e0c617e60e5464ff02dfb3ce2f16eb44f23
cuteprettykara <cuteprettykara@gmail.com>
2018-12-12 14:51:14 +0900

    주석 정리

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -1,32 +1,54 @@
 $(document).ready(function() {
     'use strict';
-    paper.install(window);
-    paper.setup(document.getElementById('mainCanvas'));
 
+    // 준비작업 : template(= boilerplate)
+    paper.install(window);                              // paper.js를 전역 스코프에 설치
+    paper.setup(document.getElementById('mainCanvas')); // paper.js를 canvas에 연결
+
+    /******************/
+    /* 원 1개만 그리기 */
+    /******************/
+    // var c = Shape.Circle(200, 200, 50);
+    // c.fillColor = 'green';
+
+
+
+    /*****************/
+    /* 원 64개 그리기 */
+    /*****************/
+    // var c;
+    // for (var x=25; x<400; x+=50) {
+    //     for (var y=25; y<400; y+=50) {
+    //         c = Shape.Circle(x, y, 20);
+    //         c.fillColor = 'green';
+    //     }
+    // }
+
+
+
+    /**********************/
+    /* 사용자 입력 처리하기 */
+    /**********************/
+    
     // 방법1
-    /* var c;
-    for (var x=25; x<400; x+=50) {
-        for (var y=25; y<400; y+=50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'green';
-        }
-    } */
-
-    var tool = new Tool();
+    // var tool = new Tool();
+    // tool.onMouseDown = function(event) {
+    //     var c = Shape.Circle(event.point.x, event.point.y, 20);
+    //     c.fillColor = 'green';
+    // }; 
 
     // 방법2
-    /* tool.onMouseDown = function(event) {
-        var c = Shape.Circle(event.point.x, event.point.y, 20);
-        c.fillColor = 'green';
-    }  */
+    // var tool = new Tool();
+    // tool.onMouseDown = function(event) {
+    //     var c = Shape.Circle(event.point, 20);
+    //     c.fillColor = 'green';
+    // };
 
-    // 방법3
-    tool.onMouseDown = function(event) {
-        var c = Shape.Circle(event.point, 20);
-        c.fillColor = 'green';
-    } 
 
-    // hello World
+
+    /***********************/
+    /* hello World 추가하기 */
+    /***********************/
     var c = Shape.Circle(200, 200, 80);
     c.fillColor = 'black';
 
@@ -35,6 +57,8 @@ $(document).ready(function() {
     text.fillColor = 'white';
     text.fontSize = 20;
     text.content = 'hello World';
+
+
 
     paper.view.draw();
 });
